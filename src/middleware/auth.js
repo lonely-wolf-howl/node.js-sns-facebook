425b48f3278e956f1918fb625bfef146c3d9794c
재용 <jaeyong@gangjingyeong-ui-MacBook-Pro.local>
2023-10-23 20:14:26 +0900

    feat. 내 정보 조회 및 수정 기능 추가 feat. header, profile front design.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,6 @@
 const Post = require('../models/posts.model');
 const Comment = require('../models/comments.model');
+const User = require('../models/users.model');
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
@@ -50,7 +51,7 @@ async function checkCommentOwnership(req, res, next) {
         res.redirect('back');
       } else {
         if (comment.author.id.equals(req.user._id)) {
-          req.comment = comment
+          req.comment = comment;
           next();
         } else {
           req.flash('error', '해당 댓글에 대한 권한이 없습니다.');
@@ -67,9 +68,35 @@ async function checkCommentOwnership(req, res, next) {
   }
 }
 
+async function checkIsMe(req, res, next) {
+  if (req.isAuthenticated()) {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        req.flash('error', '사용자가 존재하지 않습니다.');
+        res.redirect('/profile/' + req.params.id);
+      } else {
+        if (user._id.equals(req.user._id)) {
+          next();
+        } else {
+          req.flash('error', '내 정보에 대한 권한이 없습니다.');
+          res.redirect('back');
+        }
+      }
+    } catch (error) {
+      req.flash('error', '사용자 조회 중 오류가 발생했습니다.');
+      res.redirect('back');
+    }
+  } else {
+    req.flash('error', '사용자 인증이 필요합니다.');
+    res.redirect('/login');
+  }
+}
+
 module.exports = {
   checkAuthenticated,
   checkNotAuthenticated,
   checkPostOwnership,
   checkCommentOwnership,
+  checkIsMe,
 };
--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -1,4 +1,50 @@
 const express = require('express');
-const profileRouter = express.Router();
+const profileRouter = express.Router({
+  mergeParams: true,
+});
 
-module.exports = profileRouter
+const { checkAuthenticated, checkIsMe } = require('../middleware/auth');
+
+const Post = require('../models/posts.model');
+const User = require('../models/users.model');
+
+profileRouter.get('/', checkAuthenticated, async (req, res) => {
+  try {
+    const posts = await Post.find({ 'author.id': req.params.id })
+      .populate('comments')
+      .sort({ createdAt: -1 })
+      .exec();
+
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      req.flash('error', '사용자가 존재하지 않습니다.');
+      res.redirect('back');
+    }
+    res.render('profile', {
+      posts: posts,
+      user: user,
+    });
+  } catch {
+    req.flash('error', '사용자 게시물 목록 조회 중 오류가 발생했습니다.');
+    res.redirect('back');
+  }
+});
+
+profileRouter.get('/edit', checkIsMe, async (req, res) => {
+  res.render('profile/edit', {
+    user: req.user,
+  });
+});
+
+profileRouter.put('/', checkIsMe, async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(req.params.id, req.body);
+    req.flash('success', '내 정보 수정 성공.');
+    res.redirect('/profile/' + req.params.id);
+  } catch {
+    req.flash('error', '내 정보 수정 실패.');
+    res.redirect('back');
+  }
+});
+
+module.exports = profileRouter;
